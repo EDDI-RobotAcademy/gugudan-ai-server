c93da5e8d7a406babd3f0fed8725dd80831ff66e
keaunsol Na <knsol1992@naver.com>
2025-12-24 00:29:38 +0900

    refactor: [나큰솔] 감성기반 피드백을 통한 Agent 파인 튜닝 [GS-10]
    -- qdrant 관련 이슈로 주석처리

--- app/ml/application/factory/ml_usecase_factory.py
+++ app/ml/application/factory/ml_usecase_factory.py
@@ -1,20 +1,19 @@
 from app.ml.application.usecase.ml_usecase import MLUseCase
 from app.ml.infrastructure.repository.ml_repository_impl import MLRepositoryImpl
-from app.ml.infrastructure.vector_db.qdrant_impl import QdrantVectorDBImpl
 
 
 class MLUseCaseFactory:
     __vector_db_instance = None
 
-    @staticmethod
-    def _get_vector_db():
-        """Get or create Qdrant vector DB instance (singleton)."""
-        if MLUseCaseFactory.__vector_db_instance is None:
-            MLUseCaseFactory.__vector_db_instance = QdrantVectorDBImpl()
-        return MLUseCaseFactory.__vector_db_instance
+    # @staticmethod
+    # def _get_vector_db():
+    #     """Get or create Qdrant vector DB instance (singleton)."""
+        # if MLUseCaseFactory.__vector_db_instance is None:
+        #     MLUseCaseFactory.__vector_db_instance = QdrantVectorDBImpl()
+        # return MLUseCaseFactory.__vector_db_instance
 
     @staticmethod
     def create() -> MLUseCase:
         repository = MLRepositoryImpl.get_instance()
-        vector_db = MLUseCaseFactory._get_vector_db()
-        return MLUseCase(repository, vector_db)
+        # vector_db = MLUseCaseFactory._get_vector_db()
+        return MLUseCase(repository)
